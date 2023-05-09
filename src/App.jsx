8bc0672bf050855b3166284b785b84e910ef7b5c
Margarita Dagla <margaritadagla@hotmail.com>
2023-05-09 21:42:20 +0100

    created first compoent

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import ExpenseItem from './components/ExpenseItem'
 
 function App() {
   return (
     <div>
       <h2>Let's go!</h2>
+      <ExpenseItem></ExpenseItem>
     </div>
   )
 }
