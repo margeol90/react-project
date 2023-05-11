8e88c000b3797c835c19d8bcb9d5577366bc17b4
Margarita Dagla <margaritadagla@hotmail.com>
2023-05-11 20:33:37 +0100

    created multiple components

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,16 +1,14 @@
-import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate';
+import './ExpenseItem.css';
 
-function ExpenseItem() {
-  const expenseDate = new Date(2023, 4, 11);
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = 300.50;
+function ExpenseItem(props) {
 
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <ExpenseDate date= {props.date} />
       <div className='expense-item__description'>
-        <h2>{expenseTitle}</h2>
-        <div className='expense-item__price'>£{expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>£{props.amount}</div>
       </div>
     </div>
   )
