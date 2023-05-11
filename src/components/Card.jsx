e491cb44dd5fb0642018a40b259b1d778c013076
Margarita Dagla <margaritadagla@hotmail.com>
2023-05-11 21:38:47 +0100

    used css across components

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,9 @@
 import './Card.css';
 
 function Card(props) {
-  return <div className="card">{ props.children }</div>;
+  const classes = 'card ' + props.className;
+
+  return <div className={classes}>{ props.children }</div>;
 }
 
 export default Card;
